0c853be368509c96991edb86f647b4d2f31f1170
Kmathieu-nliven <kmathieu@transcendinsights.com>
2016-01-22 22:54:21 -0800

    Initialize app, add more data

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,75 +8,67 @@
  * Controller of the sandboxApp
  */
 angular.module('sandboxApp')
-  //works
-  //.controller('MainCtrl', function ($scope, $interval, Restangular) {
 
   //works
   .controller('MainCtrl', function (UsersSvc, $scope, $interval, Restangular) {
 
-  //doesn't work: Unknown provider: usersDataProvider <- usersData <- MainCtrl
-  //.controller('MainCtrl', function (usersData, UsersSvc, $scope, $interval, Restangular) {
     var vm = this;
-    vm.currentSteps = 0;
-    vm.goal = 0;
-    vm.stepBank = 0;
-    vm.currentPercentage = 0;
-    vm.hungerScore = 100;
+    vm.goal = 10000;
+    var allUsers;
+
+    vm.currentSteps;
+    vm.stepBank;
+    vm.hungerScore;
     vm.mood;
-    //vm.allUsers;
-    vm.allUsers;
-    //vm.test;
+    vm.currentPercentage;
+    vm.moreUpdatesAvail = true;
 
+    var currentUser;
 
-    var startStepTracker = function() {
+    var init = function() {
 
-      //works
-      //Restangular.all('users').getList().then(function(data){
-      //  vm.allUsers = data.plain();
-      //});
-
-      //works
       UsersSvc.getList().then(function(data){
-        vm.allUsers2 = data;
+        allUsers = data;
+      }).then(function(){
+        initializeUserData(allUsers[0]);
+
+        $interval(function () {
+          //TODO: Don't let this run during bedtime
+          vm.hungerScore -= 50;
+        }, 3000, 3);
+
       });
-
-      //doesn't work: Unknown provider: usersDataProvider <- usersData <- MainCtrl
-      //vm.test = usersData;
-
-      vm.currentSteps = 5000;
-      vm.goal = 10000;
-      vm.stepBank = vm.currentSteps + 2450;
-      vm.currentPercentage = vm.currentSteps / vm.goal *100;
-
-      $interval(function () {
-        //TODO: Don't let this run during bedtime
-        vm.hungerScore -= 50;
-      }, 3000, 3);
 
     };
 
+    function initializeUserData(initialData) {
+      vm.currentSteps = initialData.currentSteps;
+      vm.stepBank = initialData.stepBank;
+      vm.hungerScore = initialData.hungerScore;
+      vm.mood = initialData.mood;
+      vm.currentPercentage = vm.currentSteps / vm.goal *100;
+      currentUser = 0;
+    }
+
     vm.addSteps = function addSteps() {
-      vm.currentSteps += 500;
-      vm.stepBank += 500;
+      currentUser++;
+
+      vm.currentSteps =  allUsers[currentUser].currentSteps;
+      vm.stepBank =  allUsers[currentUser].stepBank;
       vm.currentPercentage = vm.currentSteps / vm.goal*100;
 
+      vm.moreUpdatesAvail = currentUser < allUsers.length-1;
     };
 
     vm.feedFitty = function feedFitty(){
       vm.stepBank -= 2000;
       vm.hungerScore += 50;
-
-
     };
 
     $scope.$watch('vm.hungerScore', function (newScore) {
       vm.mood = newScore <= 50 ? 'Hungry' : 'Happy';
 
     });
-
-    var init = function() {
-      startStepTracker();
-    };
 
     function setMood() { //Use this later?
       var moodOptions = ['Hungry', 'Hangry', 'Thirsty', 'Bored', 'Lonely', 'Happy'];
