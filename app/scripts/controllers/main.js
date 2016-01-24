02e7ab2587035956194d2ee55b8c8a320c097009
Kmathieu-nliven <kmathieu@transcendinsights.com>
2016-01-24 15:27:12 -0800

    Added actions for hunger, happiness, mood

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -16,26 +16,43 @@ angular.module('sandboxApp')
     vm.goal = 10000;
     var allUsers;
 
+    //currency
     vm.currentSteps;
     vm.stepBank;
-    vm.hungerScore;
+
+    //life scores
+    vm.hunger = {};
+    vm.happiness = {};
+    //vm.hunger.text;
+    //vm.happiness.text;
     vm.mood;
+    vm.isSick;
+    vm.isDead;
+
     vm.currentPercentage;
     vm.moreUpdatesAvail = true;
 
     var currentUser;
 
-    var init = function() {
+    var mealCost = .2;
+    var playCost = .1;
 
-      UsersSvc.getList().then(function(data){
+    var init = function () {
+
+      UsersSvc.getList().then(function (data) {
         allUsers = data;
-      }).then(function(){
+      }).then(function () {
         initializeUserData(allUsers[0]);
 
         $interval(function () {
           //TODO: Don't let this run during bedtime
-          vm.hungerScore -= 50;
+          vm.hunger.score -= 1;
         }, 3000, 3);
+
+        $interval(function () {
+          //TODO: Don't let this run during bedtime
+          vm.happiness.score -= 1;
+        }, 4000, 3);
 
       });
 
@@ -44,35 +61,95 @@ angular.module('sandboxApp')
     function initializeUserData(initialData) {
       vm.currentSteps = initialData.currentSteps;
       vm.stepBank = initialData.stepBank;
-      vm.hungerScore = initialData.hungerScore;
+      vm.hunger.score = initialData.hunger.score;
+      vm.happiness.score = initialData.happiness.score;
       vm.mood = initialData.mood;
-      vm.currentPercentage = vm.currentSteps / vm.goal *100;
+      vm.isSick = initialData.isSick;
+      vm.isDead = initialData.isDead;
+
+      vm.currentPercentage = vm.currentSteps / vm.goal * 100;
       currentUser = 0;
+
+      setMood();
     }
 
     vm.addSteps = function addSteps() {
       currentUser++;
 
-      vm.currentSteps =  allUsers[currentUser].currentSteps;
-      vm.stepBank =  allUsers[currentUser].stepBank;
-      vm.currentPercentage = vm.currentSteps / vm.goal*100;
+      vm.currentSteps = allUsers[currentUser].currentSteps;
+      vm.stepBank = allUsers[currentUser].stepBank;
+      vm.currentPercentage = vm.currentSteps / vm.goal * 100;
 
-      vm.moreUpdatesAvail = currentUser < allUsers.length-1;
+      vm.moreUpdatesAvail = currentUser < allUsers.length - 1;
     };
 
-    vm.feedFitty = function feedFitty(){
-      vm.stepBank -= 2000;
-      vm.hungerScore += 50;
+    //todo:  add play with fitty
+    //todo:  add checks to not go in negatives
+    vm.feedFitty = function feedFitty() {
+      vm.stepBank -= mealCost * vm.goal;
+      vm.hunger.score += 1;
     };
 
-    $scope.$watch('vm.hungerScore', function (newScore) {
-      vm.mood = newScore <= 50 ? 'Hungry' : 'Happy';
+    //$scope.$watch('vm.hunger.score', function (newHungerScore) {
+    $scope.$watchGroup(['vm.hunger.score', 'vm.happiness.score'], function (newValues, oldValues, scope) {
+
+      if (newValues[0] != oldValues[0]) {
+        var newHungerScore = newValues[0];
+        switch (newHungerScore) {
+          case 0:
+            vm.isDead = true;
+            break;
+          case 1:
+            vm.isDead = false;
+            //vm.happiness.score -= 1;  //TODO: FIGURE THIS OUT
+            vm.hunger.text = 'Starving';
+            break;
+          case 2:
+            vm.isDead = false;
+            vm.hunger.text = 'Hungry';
+            break;
+          default:
+            vm.isDead = false;
+            vm.hunger.text = '';
+        }
+      }
+      if (newValues[1] != oldValues[1]) {
+        var newHappinessScore = newValues[1];
+
+        switch (newHappinessScore) {
+          case 0:
+            vm.isDead = true;
+            break;
+          case 1:
+            vm.isDead = false;
+            vm.happiness.text = 'Sad';
+            break;
+          default:
+            vm.isDead = false;
+            vm.happiness.text = '';
+        }
+
+      }
+
+      setMood();
+
 
     });
 
     function setMood() { //Use this later?
       var moodOptions = ['Hungry', 'Hangry', 'Thirsty', 'Bored', 'Lonely', 'Happy'];
-      //TODO:  maybe set this as object with type, label, and scores associated to trigger?
+      //TODO:  set multiple options for different happiness/hunger levels to keep it interesting
+
+      if (vm.isSick) {
+        vm.mood = "Sick";
+      }
+      else if ((vm.hunger.text) && (vm.happiness.text)) {
+        vm.mood = vm.hunger.text + " and " + vm.happiness.text;
+      } else if ((vm.hunger.text) || (vm.happiness.text)) {
+        vm.mood = vm.hunger.text.concat(vm.happiness.text);
+      } else {
+        vm.mood = "Elated";
+      }
 
     }
 
